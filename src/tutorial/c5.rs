aabb5d4ce356827b3eb1d728505fb75011b71aed
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-02-28 13:54:47 +0900

    feat:simple

--- src/tutorial/c5.rs
+++ src/tutorial/c5.rs
@@ -1,8 +1,156 @@
+use candle_core::{DType, Device, Result, Tensor, D};
+use candle_nn::{loss, ops, Linear, Module, Optimizer, VarBuilder, VarMap};
 /*1차 선거 결과 기반으로 2차선거의 승자를 예측
 1.두개의 은닉충을 갖는 다층 퍼셉트론 사용 첫번째 은닉충에는4개의 뉴런 2번쨰 은닉충에는 2개의 뉴런
+2.2개의 숫자로 구성된 벡터
+3.0또는 1
+4.경사하강법.교차엔트로피손실함수
 
 
 
 
 */
 
+const VOTE_DIM: usize = 2;//2차원 벡터
+const RESULTS: usize = 1;//모델이; 예측하는 개수
+const EPOCHS: usize = 10;//에폭
+const LAYER1_OUT_SIZE: usize = 4;//첫번쨰 출력충의 출력뉴런 수
+const LAYER2_OUT_SIZE: usize = 2;//2번쨰 츨략층의  출력 뉴런 수
+const LEARNING_RATE: f64 = 0.05;
+#[derive(Clone)]
+pub struct Dataset {
+    pub train_votes: Tensor,//train data
+    pub train_results: Tensor,
+    pub test_votes: Tensor,//test data
+    pub test_results: Tensor,
+}
+
+struct MultiLevelPerceptron {
+    ln1: Linear,
+    ln2: Linear,//은닉충
+    ln3: Linear,//출력충
+}
+//3개 => 2개의 은닉충 1개의 출력충
+impl MultiLevelPerceptron {
+    fn new(vs: VarBuilder) -> candle_core::Result<Self> {
+        let ln1 = candle_nn::linear(VOTE_DIM, LAYER1_OUT_SIZE, vs.pp("ln1"))?;
+        let ln2 = candle_nn::linear(LAYER1_OUT_SIZE, LAYER2_OUT_SIZE, vs.pp("ln2"))?;
+        let ln3 = candle_nn::linear(LAYER2_OUT_SIZE, RESULTS + 1, vs.pp("ln3"))?;
+        Ok(Self { ln1, ln2, ln3 })
+    }
+    fn forward(&self, xs: &Tensor) -> candle_core::Result<Tensor> {
+        let xs = self.ln1.forward(xs)?;
+        let xs = xs.relu()?;
+        let xs = self.ln2.forward(&xs)?;
+        let xs = xs.relu()?;
+        self.ln3.forward(&xs)
+    }
+}
+
+fn train(m: Dataset, dev: &Device) -> anyhow::Result<MultiLevelPerceptron> {
+    let train_results = m.train_results.to_device(dev)?; //디바이스
+    let train_votes = m.train_votes.to_device(dev)?;
+    let varmap = VarMap::new();//VarMap은 변수들을 관리하는 데 사용되는 자료 구조
+    let vs = VarBuilder::from_varmap(&varmap, DType::F32, dev);
+    let model = MultiLevelPerceptron::new(vs.clone())?;
+    let mut sgd = candle_nn::SGD::new(varmap.all_vars(), LEARNING_RATE)?;
+    let test_votes = m.test_votes.to_device(dev)?;
+    let test_results = m.test_results.to_device(dev)?;
+    let mut final_accuracy: f32 = 0.;
+    for epoch in 1..EPOCHS + 1 {
+        let logits = model.forward(&train_votes)?;
+        let log_sm = ops::log_softmax(&logits, D::Minus1)?;//Minus1:가장 마지막 축
+        let loss = loss::nll(&log_sm, &train_results)?;//손실함수
+        sgd.backward_step(&loss)?;//역전파
+        let test_logits = model.forward(&test_votes)?;
+        let sum_ok = test_logits
+            .argmax(D::Minus1)?
+            .eq(&test_results)?
+            .to_dtype(DType::F32)?
+            .sum_all()?
+            .to_scalar::<f32>()?;//정확도 계산
+        let test_accuracy = sum_ok / test_results.dims1()? as f32;
+        final_accuracy = 100. * test_accuracy;
+        println!(
+            "Epoch: {epoch:3} Train loss: {:8.5} Test accuracy: {:5.2}%",
+            loss.to_scalar::<f32>()?,
+            final_accuracy
+        );
+        if final_accuracy == 100.0 {
+            break;
+        }
+    }
+    if final_accuracy < 100.0 {
+        Err(anyhow::Error::msg("The model is not trained well enough."))
+    } else {
+        Ok(model)
+    }
+}
+#[tokio::main]
+pub async fn c5() -> anyhow::Result<()> {
+    let dev = Device::cuda_if_available(0)?;
+
+    let train_votes_vec: Vec<u32> =
+        vec![15, 10, 10, 15, 5, 12, 30, 20, 16, 12, 13, 25, 6, 14, 31, 21];
+    let train_votes_tensor = Tensor::from_vec(
+        train_votes_vec.clone(),
+        (train_votes_vec.len() / VOTE_DIM, VOTE_DIM),
+        &dev,
+    )?
+    .to_dtype(DType::F32)?;
+
+    let train_results_vec: Vec<u32> = vec![1, 0, 0, 1, 1, 0, 0, 1];
+    let train_results_tensor =
+        Tensor::from_vec(train_results_vec, train_votes_vec.len() / VOTE_DIM, &dev)?;
+
+    let test_votes_vec: Vec<u32> = vec![13, 9, 8, 14, 3, 10];
+    let test_votes_tensor = Tensor::from_vec(
+        test_votes_vec.clone(),
+        (test_votes_vec.len() / VOTE_DIM, VOTE_DIM),
+        &dev,
+    )?
+    .to_dtype(DType::F32)?;
+
+    let test_results_vec: Vec<u32> = vec![1, 0, 0];
+    let test_results_tensor =
+        Tensor::from_vec(test_results_vec.clone(), test_results_vec.len(), &dev)?;
+
+    let m = Dataset {
+        train_votes: train_votes_tensor,
+        train_results: train_results_tensor,
+        test_votes: test_votes_tensor,
+        test_results: test_results_tensor,
+    };
+
+    let trained_model: MultiLevelPerceptron;
+    loop {
+        println!("Trying to train neural network.");
+        match train(m.clone(), &dev) {
+            Ok(model) => {
+                trained_model = model;
+                break;
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                continue;
+            }
+        }
+    }
+    //추정
+    let real_world_votes: Vec<u32> = vec![13, 22];
+
+    let tensor_test_votes =
+        Tensor::from_vec(real_world_votes.clone(), (1, VOTE_DIM), &dev)?.to_dtype(DType::F32)?;
+
+    let final_result = trained_model.forward(&tensor_test_votes)?;
+
+    let result = final_result
+        .argmax(D::Minus1)?
+        .to_dtype(DType::F32)?
+        .get(0)
+        .map(|x| x.to_scalar::<f32>())??;
+    println!("real_life_votes: {:?}", real_world_votes);
+    println!("neural_network_prediction_result: {:?}", result);
+
+    Ok(())
+}
--- src/tutorial/mod.rs
+++ src/tutorial/mod.rs
@@ -4,6 +4,7 @@ pub mod c2;
 pub mod c3;
 pub mod c4;
 pub mod c5;
+pub mod mnist;
 pub fn main() {
     c5::c5().unwrap()
 }
