e4cab88610209dab13269b70ff12b158ef4cda4d
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-02-29 11:13:34 +0900

    feat:mnist test

--- src/tutorial/mnist.rs
+++ src/tutorial/mnist.rs
@@ -2,7 +2,6 @@ use candle_core::{DType, Device, Result, Tensor, D};
 use candle_nn::{loss, ops, Linear, Module, Optimizer, VarBuilder, VarMap};
 use polars::prelude::*;
 
-
 const IMAGE_DIM: usize = 784; //2차원 벡터
 const RESULTS: usize = 10; //모델이; 예측하는 개수
 const EPOCHS: usize = 10; //에폭
@@ -10,14 +9,84 @@ const LAYER1_OUT_SIZE: usize = 4; //첫번쨰 출력충의 출력뉴런 수
 const LAYER2_OUT_SIZE: usize = 2; //2번쨰 츨략층의  출력 뉴런 수
 const LEARNING_RATE: f64 = 0.05;
 
-#[derive(Clone)]
+#[derive(Clone,Debug)]
 pub struct Dataset {
     pub train_images: Tensor, //train data
     pub train_labels: Tensor,
     pub test_images: Tensor, //test data
     pub test_labels: Tensor,
 }
+impl Dataset {
+    fn new() -> candle_core::Result<Self> {
+        let train_samples = 42_000;
+        let test_samples = 28_000;
+        //데이터불러오기
+        let train_df = CsvReader::from_path("./dataset/digit-recognizer/train.csv")
+            .unwrap()
+            .finish()
+            .unwrap();
+        let test_df = CsvReader::from_path("./dataset/digit-recognizer/test.csv")
+            .unwrap()
+            .finish()
+            .unwrap();
+        let submission_df = CsvReader::from_path("./dataset/digit-recognizer/sample_submission.csv")
+            .unwrap()
+            .finish()
+            .unwrap();
 
+        let labels = train_df
+            .column("label")
+            .unwrap()
+            .i64()
+            .unwrap()
+            .into_no_null_iter()
+            .map(|x| x as u8)
+            .collect::<Vec<u8>>();
+        let test_labels = submission_df
+            .column("Label")
+            .unwrap()
+            .i64()
+            .unwrap()
+            .into_no_null_iter()
+            .map(|x| x as u8)
+            .collect::<Vec<u8>>();
+
+        let train_labels = Tensor::from_vec(labels, (train_samples,), &Device::Cpu)?;
+        let test_labels = Tensor::from_vec(test_labels, (test_samples,), &Device::Cpu)?;
+
+        let x_test = test_df
+            .to_ndarray::<Int64Type>(IndexOrder::Fortran)
+            .unwrap();
+        let mut test_buffer_images: Vec<u8> = Vec::with_capacity(test_samples * 784);
+        for i in x_test {
+            test_buffer_images.push(i as u8)
+        }
+        let test_images =
+            (Tensor::from_vec(test_buffer_images, (test_samples, 784), &Device::Cpu)?
+                .to_dtype(DType::F32)?
+                / 255.)?;
+
+        let x_train = train_df
+            .drop("label")
+            .unwrap()
+            .to_ndarray::<Int64Type>(IndexOrder::Fortran)
+            .unwrap();
+        let mut train_buffer_images: Vec<u8> = Vec::with_capacity(train_samples * 784);
+        for i in x_train {
+            train_buffer_images.push(i as u8)
+        }
+        let train_images =
+            (Tensor::from_vec(train_buffer_images, (train_samples, 784), &Device::Cpu)?
+                .to_dtype(DType::F32)?
+                / 255.)?;
+        Ok(Self {
+            train_images: train_images,
+            train_labels: train_labels,
+            test_images: test_images,
+            test_labels: test_labels,
+        })
+    }
+}
 struct MultiLevelPerceptron {
     ln1: Linear,
     ln2: Linear, //은닉충
@@ -82,21 +151,35 @@ fn train(m: Dataset, dev: &Device) -> anyhow::Result<MultiLevelPerceptron> {
 #[tokio::main]
 pub async fn model() -> anyhow::Result<()> {
     let dev = Device::cuda_if_available(0)?;
-    let mut test_df = CsvReader::from_path("./dataset/digit-recognizer/test.csv")
-        .unwrap()
-        .finish()
-        .unwrap();
-    let y_test = test_df
-        .to_ndarray::<Int64Type>(IndexOrder::Fortran)
-        .unwrap();
-    let test_samples = 28_000;
-    let mut test_buffer_images: Vec<u8> = Vec::with_capacity(test_samples * 784);
-    for i in y_test {
-        test_buffer_images.push(i as u8)
+    let m = Dataset::new()?;
+    let trained_model: MultiLevelPerceptron;
+    loop {
+        println!("Trying to train neural network.");
+        match train(m.clone(), &dev) {
+            Ok(model) => {
+                trained_model = model;
+                break;
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                continue;
+            }
+        }
     }
-    let test_images = (Tensor::from_vec(test_buffer_images, (test_samples, 784), &Device::Cpu)?
+    //추정
+    let real_world_votes: Vec<u32> = vec![13, 22];
+
+    let tensor_test_votes =
+        Tensor::from_vec(real_world_votes.clone(), (1, IMAGE_DIM), &dev)?.to_dtype(DType::F32)?;
+
+    let final_result = trained_model.forward(&tensor_test_votes)?;
+
+    let result = final_result
+        .argmax(D::Minus1)?
         .to_dtype(DType::F32)?
-        / 255.)?;
-    println!("{}", test_images);
+        .get(0)
+        .map(|x| x.to_scalar::<f32>())??;
+    println!("real_life_votes: {:?}", real_world_votes);
+    println!("neural_network_prediction_result: {:?}", result);
     Ok(())
 }
