c017e462257aeed60195ce3fcd9187a97ea3acee
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-02-29 10:48:04 +0900

    feat:test_df

--- src/tutorial/mnist.rs
+++ src/tutorial/mnist.rs
@@ -1,39 +1,32 @@
 use candle_core::{DType, Device, Result, Tensor, D};
 use candle_nn::{loss, ops, Linear, Module, Optimizer, VarBuilder, VarMap};
-/*1차 선거 결과 기반으로 2차선거의 승자를 예측
-1.두개의 은닉충을 갖는 다층 퍼셉트론 사용 첫번째 은닉충에는4개의 뉴런 2번쨰 은닉충에는 2개의 뉴런
-2.2개의 숫자로 구성된 벡터
-3.0또는 1
-4.경사하강법.교차엔트로피손실함수
+use polars::prelude::*;
 
 
-
-
-*/
-
-const VOTE_DIM: usize = 2;//2차원 벡터
-const RESULTS: usize = 1;//모델이; 예측하는 개수
-const EPOCHS: usize = 10;//에폭
-const LAYER1_OUT_SIZE: usize = 4;//첫번쨰 출력충의 출력뉴런 수
-const LAYER2_OUT_SIZE: usize = 2;//2번쨰 츨략층의  출력 뉴런 수
+const IMAGE_DIM: usize = 784; //2차원 벡터
+const RESULTS: usize = 10; //모델이; 예측하는 개수
+const EPOCHS: usize = 10; //에폭
+const LAYER1_OUT_SIZE: usize = 4; //첫번쨰 출력충의 출력뉴런 수
+const LAYER2_OUT_SIZE: usize = 2; //2번쨰 츨략층의  출력 뉴런 수
 const LEARNING_RATE: f64 = 0.05;
+
 #[derive(Clone)]
 pub struct Dataset {
-    pub train_votes: Tensor,//train data
-    pub train_results: Tensor,
-    pub test_votes: Tensor,//test data
-    pub test_results: Tensor,
+    pub train_images: Tensor, //train data
+    pub train_labels: Tensor,
+    pub test_images: Tensor, //test data
+    pub test_labels: Tensor,
 }
 
 struct MultiLevelPerceptron {
     ln1: Linear,
-    ln2: Linear,//은닉충
-    ln3: Linear,//출력충
+    ln2: Linear, //은닉충
+    ln3: Linear, //출력충
 }
 //3개 => 2개의 은닉충 1개의 출력충
 impl MultiLevelPerceptron {
     fn new(vs: VarBuilder) -> candle_core::Result<Self> {
-        let ln1 = candle_nn::linear(VOTE_DIM, LAYER1_OUT_SIZE, vs.pp("ln1"))?;
+        let ln1 = candle_nn::linear(IMAGE_DIM, LAYER1_OUT_SIZE, vs.pp("ln1"))?;
         let ln2 = candle_nn::linear(LAYER1_OUT_SIZE, LAYER2_OUT_SIZE, vs.pp("ln2"))?;
         let ln3 = candle_nn::linear(LAYER2_OUT_SIZE, RESULTS + 1, vs.pp("ln3"))?;
         Ok(Self { ln1, ln2, ln3 })
@@ -48,27 +41,27 @@ impl MultiLevelPerceptron {
 }
 
 fn train(m: Dataset, dev: &Device) -> anyhow::Result<MultiLevelPerceptron> {
-    let train_results = m.train_results.to_device(dev)?; //디바이스
-    let train_votes = m.train_votes.to_device(dev)?;
-    let varmap = VarMap::new();//VarMap은 변수들을 관리하는 데 사용되는 자료 구조
+    let train_results = m.train_labels.to_device(dev)?; //디바이스
+    let train_votes = m.train_images.to_device(dev)?;
+    let varmap = VarMap::new(); //VarMap은 변수들을 관리하는 데 사용되는 자료 구조
     let vs = VarBuilder::from_varmap(&varmap, DType::F32, dev);
     let model = MultiLevelPerceptron::new(vs.clone())?;
     let mut sgd = candle_nn::SGD::new(varmap.all_vars(), LEARNING_RATE)?;
-    let test_votes = m.test_votes.to_device(dev)?;
-    let test_results = m.test_results.to_device(dev)?;
+    let test_votes = m.test_images.to_device(dev)?;
+    let test_results = m.test_labels.to_device(dev)?;
     let mut final_accuracy: f32 = 0.;
     for epoch in 1..EPOCHS + 1 {
         let logits = model.forward(&train_votes)?;
-        let log_sm = ops::log_softmax(&logits, D::Minus1)?;//Minus1:가장 마지막 축
-        let loss = loss::nll(&log_sm, &train_results)?;//손실함수
-        sgd.backward_step(&loss)?;//역전파
+        let log_sm = ops::log_softmax(&logits, D::Minus1)?; //Minus1:가장 마지막 축
+        let loss = loss::nll(&log_sm, &train_results)?; //손실함수
+        sgd.backward_step(&loss)?; //역전파
         let test_logits = model.forward(&test_votes)?;
         let sum_ok = test_logits
             .argmax(D::Minus1)?
             .eq(&test_results)?
             .to_dtype(DType::F32)?
             .sum_all()?
-            .to_scalar::<f32>()?;//정확도 계산
+            .to_scalar::<f32>()?; //정확도 계산
         let test_accuracy = sum_ok / test_results.dims1()? as f32;
         final_accuracy = 100. * test_accuracy;
         println!(
@@ -89,68 +82,21 @@ fn train(m: Dataset, dev: &Device) -> anyhow::Result<MultiLevelPerceptron> {
 #[tokio::main]
 pub async fn model() -> anyhow::Result<()> {
     let dev = Device::cuda_if_available(0)?;
-
-    let train_votes_vec: Vec<u32> =
-        vec![15, 10, 10, 15, 5, 12, 30, 20, 16, 12, 13, 25, 6, 14, 31, 21];
-    let train_votes_tensor = Tensor::from_vec(
-        train_votes_vec.clone(),
-        (train_votes_vec.len() / VOTE_DIM, VOTE_DIM),
-        &dev,
-    )?
-    .to_dtype(DType::F32)?;
-
-    let train_results_vec: Vec<u32> = vec![1, 0, 0, 1, 1, 0, 0, 1];
-    let train_results_tensor =
-        Tensor::from_vec(train_results_vec, train_votes_vec.len() / VOTE_DIM, &dev)?;
-
-    let test_votes_vec: Vec<u32> = vec![13, 9, 8, 14, 3, 10];
-    let test_votes_tensor = Tensor::from_vec(
-        test_votes_vec.clone(),
-        (test_votes_vec.len() / VOTE_DIM, VOTE_DIM),
-        &dev,
-    )?
-    .to_dtype(DType::F32)?;
-
-    let test_results_vec: Vec<u32> = vec![1, 0, 0];
-    let test_results_tensor =
-        Tensor::from_vec(test_results_vec.clone(), test_results_vec.len(), &dev)?;
-
-    let m = Dataset {
-        train_votes: train_votes_tensor,
-        train_results: train_results_tensor,
-        test_votes: test_votes_tensor,
-        test_results: test_results_tensor,
-    };
-
-    let trained_model: MultiLevelPerceptron;
-    loop {
-        println!("Trying to train neural network.");
-        match train(m.clone(), &dev) {
-            Ok(model) => {
-                trained_model = model;
-                break;
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-                continue;
-            }
-        }
+    let mut test_df = CsvReader::from_path("./dataset/digit-recognizer/test.csv")
+        .unwrap()
+        .finish()
+        .unwrap();
+    let y_test = test_df
+        .to_ndarray::<Int64Type>(IndexOrder::Fortran)
+        .unwrap();
+    let test_samples = 28_000;
+    let mut test_buffer_images: Vec<u8> = Vec::with_capacity(test_samples * 784);
+    for i in y_test {
+        test_buffer_images.push(i as u8)
     }
-    //추정
-    let real_world_votes: Vec<u32> = vec![13, 22];
-
-    let tensor_test_votes =
-        Tensor::from_vec(real_world_votes.clone(), (1, VOTE_DIM), &dev)?.to_dtype(DType::F32)?;
-
-    let final_result = trained_model.forward(&tensor_test_votes)?;
-
-    let result = final_result
-        .argmax(D::Minus1)?
+    let test_images = (Tensor::from_vec(test_buffer_images, (test_samples, 784), &Device::Cpu)?
         .to_dtype(DType::F32)?
-        .get(0)
-        .map(|x| x.to_scalar::<f32>())??;
-    println!("real_life_votes: {:?}", real_world_votes);
-    println!("neural_network_prediction_result: {:?}", result);
-
+        / 255.)?;
+    println!("{}", test_images);
     Ok(())
 }
