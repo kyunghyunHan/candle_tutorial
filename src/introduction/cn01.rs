647f4efd60656bbaeb50d26012fcdbd65bb2b3a6
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-03-11 17:49:00 +0900

    feat:test

--- src/introduction/cn01.rs
+++ src/introduction/cn01.rs
@@ -1,7 +1,7 @@
 use candle_core::{DType, Device, Tensor,D};
 use candle_nn::{loss, Linear, Module, Optimizer, VarBuilder, VarMap, SGD,ops};
 const LEARNING_RATE: f64 = 0.01;
-const EPOCHS: usize = 1999;
+const EPOCHS: usize = 2000;
 
 struct MultiLevelPerceptron {
     ln1: Linear,
@@ -22,26 +22,20 @@ pub async fn main() -> anyhow::Result<()> {
     let device = Device::Cpu;
     let x_train = Tensor::new(&[[1.], [2.], [3.]], &device)?; // 입력 데이터를 1차원으로 변경
     let y_train = Tensor::new(&[[2.], [4.], [6.]], &device)?;
-
-    //  let w: Tensor = Tensor::zeros(1, candle_core::DType::F64, &device)?;
-    //  let b = Tensor::zeros(1, candle_core::DType::F64, &device)?;
-    //  // // println!("{:?}",w.shape());
-    //  // println!("{:?}",x_train.shape());
     let varmap = VarMap::new();
     let vs = VarBuilder::from_varmap(&varmap, DType::F64, &device);
     let model = MultiLevelPerceptron::new(vs.clone())?;
     let mut sgd = candle_nn::SGD::new(varmap.all_vars(), LEARNING_RATE)?;
-
     for epoch in 0..EPOCHS + 1 {
         let logits = model.forward(&x_train)?;
-
+   
         let loss = loss::mse(&logits, &y_train)?;
+      
         sgd.backward_step(&loss)?;
-
         if epoch % 100 == 0 {
-            println!("Epoch {:.4?}", loss.to_scalar::<f64>()?)
+            println!("Epoch {}/{} Cost : {:.6}",EPOCHS,epoch, loss.to_scalar::<f64>()?)
         }
     }
-    // }
     Ok(())
 }
+ 
