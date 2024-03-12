ae05fe129c53350937518af60dd6ca7efd88505d
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-03-12 21:33:24 +0900

    feat:test

--- src/introduction/cn03.rs
+++ src/introduction/cn03.rs
@@ -1,10 +1,25 @@
-use candle_core::{Tensor,Result,Device};
+use candle_core::{Device, Result, Tensor,DType};
+use candle_nn::{VarBuilder,VarMap,Optimizer,SGD};
+const LEARNING_RATE:f64= 0.1;
+pub fn main() -> candle_core::Result<()> {
+    let device = Device::Cpu;
+    let x1_train = Tensor::new(&[[73.], [93.], [89.], [96.], [73.]], &device)?;
+    let x2_train = Tensor::new(&[[80.], [88.], [91.], [98.], [66.]], &device)?;
+    let x3_train = Tensor::new(&[[75.], [93.], [90.], [100.], [70.]], &device)?;
+    let y_train = Tensor::new(&[[152.], [185.], [180.], [196.], [142.]], &device)?;
 
-pub fn main()->candle_core::Result<()>{
-    let device= Device::Cpu;
-    let x_train= Tensor::new(&[[73.], [93.], [89.], [96.], [73.]], &device)?;
-
-    println!("{:?}",x_train);
-
+    let w1 = Tensor::zeros(1, candle_core::DType::F64, &device)?;
+    let w2 = Tensor::zeros(1, candle_core::DType::F32, &device)?;
+    let w3 = Tensor::zeros(1, candle_core::DType::F32, &device)?;
+    let wb = Tensor::zeros(1, candle_core::DType::F32, &device)?;
+    
+    let varmap = VarMap::new();
+    let vs = VarBuilder::from_varmap(&varmap, DType::F64, &device);
+    let mut sgd = candle_nn::SGD::new(varmap.all_vars(), LEARNING_RATE)?;
+    let nb_epoch= 1000;
+    for epoch in 0..=nb_epoch{
+        let a= x1_train.broadcast_matmul(&w1.unsqueeze(1)?)?;
+        println!("{}",a);
+    }
     Ok(())
 }
