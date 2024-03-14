5739ead813aedbbcbe25a61ce247d436e207df78
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-03-14 18:15:52 +0900

    feat:softmax

--- src/introduction/cn03.rs
+++ src/introduction/cn03.rs
@@ -2,8 +2,9 @@
 use candle_core::{DType, Device, Tensor, D};
 use candle_nn::{loss, ops, Linear, Module, Optimizer, VarBuilder, VarMap, SGD};
 use candle_datasets::{Batcher};
-const LEARNING_RATE: f64 = 1e-5;
-const EPOCHS: usize = 2000;
+use polars::datatypes::DataType;
+const LEARNING_RATE: f64 = 1.;
+const EPOCHS: usize = 1000;
 
 struct MultiLevelPerceptron {
     ln1: Linear,
@@ -11,52 +12,49 @@ struct MultiLevelPerceptron {
 
 impl MultiLevelPerceptron {
     fn new(vs: VarBuilder) -> candle_core::Result<Self> {
-        let ln1 = candle_nn::linear(3, 1, vs.pp("ln1"))?;
+        let ln1 = candle_nn::linear(2, 1, vs.pp("ln1"))?;
         Ok(Self { ln1 })
     }
 
     fn forward(&self, xs: &Tensor) -> candle_core::Result<Tensor> {
-        self.ln1.forward(xs)
+       let xs= self.ln1.forward(xs)?;
+       candle_nn::ops::sigmoid(&xs)
+        
     }
 }
 #[tokio::main]
 pub async fn main() -> anyhow::Result<()> {
     let device = Device::Cpu;
     let x_train = Tensor::new(
-        &[
-            [73., 80., 75.],
-            [93., 88., 93.],
-            [89., 91., 90.],
-            [96., 98., 100.],
-            [73., 66., 70.],
-        ],
+        &[[1., 2.], [2., 3.], [3., 1.], [4., 3.], [5., 3.], [6., 2.]],
         &device,
     )?;
-    let y_train = Tensor::new(&[[152.], [185.], [180.], [196.], [142.]], &device)?;
+    let y_train = Tensor::new(& [[0.], [0.], [0.], [1.], [1.], [1.]], &device)?;
     let varmap = VarMap::new();
     let vs = VarBuilder::from_varmap(&varmap, DType::F64, &device);
     let model = MultiLevelPerceptron::new(vs.clone())?;
     let mut sgd = candle_nn::SGD::new(varmap.all_vars(), LEARNING_RATE)?;
     for epoch in 0..EPOCHS + 1 {
-        let prediction = model.forward(&x_train)?;
-
-        let loss = loss::mse(&prediction, &y_train)?;
-
+        let hypothesis = model.forward(&x_train)?;
+        let loss = loss::binary_cross_entropy_with_logit(&hypothesis, &y_train)?;
         sgd.backward_step(&loss)?;
 
 
-    if epoch % 100 == 0 {
-            println!(
-                "Epoch {}/{} Cost : {:.6}",
-                EPOCHS,
-                epoch,
-                loss.to_scalar::<f64>()?
-            )
+    if epoch % 10 == 0 {
+        let ge_t = Tensor::new(
+            0.5,
+            &device,
+        )?;
+        //>=
+        let prediction = hypothesis.broadcast_ge(&ge_t)?;
+        let correct_prediction = prediction.to_dtype(DType::F64)?.eq(&y_train)?;
+        let accuracy = correct_prediction.to_dtype(DType::F64)?.sum_all()?.to_scalar::<f64>()?/correct_prediction.shape().dims()[0] as f64;
+        println!("Epoch {}/{} cost:{} Accuracy{}% ",epoch,EPOCHS,loss.to_scalar::<f64>()?,accuracy*100.0)
+       
+
+
         }
     }
-    let new = Tensor::new(&[[73., 80., 75.]], &device)?;
-    let pred_y =model.forward(&new)?;
-    println!("{}",pred_y);
 
     Ok(())
 }
