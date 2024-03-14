a7cc1b793bb8eb8e92f0eb44bc9bd1c5ad76ed35
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-03-14 18:33:20 +0900

    feat:cn serise

--- src/introduction/cn04.rs
+++ src/introduction/cn04.rs
@@ -1,12 +1,17 @@
 /*소프트맥스 */
-use candle_core::{Tensor,Device};
+use candle_core::{Tensor,Device,DType};
 use candle_nn::{Optimizer,ops};
 #[tokio::main]
 pub async fn main() -> anyhow::Result<()> {
     let  z = Tensor::new(&[1.,2.,3.], &Device::Cpu)?;
     let htpothesis=ops::softmax(&z, 0)?;
     println!("{}",htpothesis);
-  
+    
 
+    println!("{}",htpothesis.sum(0)?);
+    
+    let z= Tensor::rand(1.0, 2.0, &[3,5], &Device::Cpu)?;
+    let htpothesis=ops::softmax(&z, 0)?;
+    println!("{}",htpothesis);
     Ok(())
 }
