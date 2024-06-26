97917631d8e594da27e919c3719530257fd61dfc
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-06-26 22:31:27 +0900

    feat:test

--- src/lstm/mod.rs
+++ src/lstm/mod.rs
@@ -2,17 +2,17 @@ use candle_core::Result;
 use polars::prelude::{CsvReadOptions, CsvReader, SerReader,LazyFrame,LazyCsvReader,LazyFileListReader};
 
 pub fn main() -> Result<()> {
-    let lf = LazyCsvReader::new("./dataset/ratings_train.csv").with_truncate_ragged_lines(true).finish().unwrap().collect().unwrap();
+    let df = LazyCsvReader::new("./dataset/ratings_test.csv")
+    .with_truncate_ragged_lines(true)
+    .with_ignore_errors(true) // 파싱 오류 무시
+    .with_rechunk(true)
+    .with_skip_rows_after_header(0)
+    .finish()
+    .unwrap()
+    .collect()
+    .unwrap();    
+println!("{:?}",df.height());
+println!("{:?}",df.head(Some(10)));
 
-    // let a = CsvReadOptions::default()
-    //     .with_has_header(true)
-    
-    //     .try_into_reader_with_file_path(Some("./dataset/ratings_train.csv".into()))
-        
-    //     .unwrap()
-        
-    //     .finish()
-    //     .unwrap();
-    println!("{:?}",lf);
     Ok(())
 }
