3b83e43ae835675a0829dadf5f210024b6782af3
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-06-27 23:37:20 +0900

    feat:csv reader

--- src/lstm/mod.rs
+++ src/lstm/mod.rs
@@ -1,18 +1,27 @@
 use candle_core::Result;
-use polars::prelude::{CsvReadOptions, CsvReader, SerReader,LazyFrame,LazyCsvReader,LazyFileListReader};
+use polars::prelude::*;
 
 pub fn main() -> Result<()> {
-    let df = LazyCsvReader::new("./dataset/ratings_test.csv")
-    .with_truncate_ragged_lines(true)
-    .with_ignore_errors(true) // 파싱 오류 무시
-    .with_rechunk(true)
-    .with_skip_rows_after_header(0)
-    .finish()
-    .unwrap()
-    .collect()
-    .unwrap();    
-println!("{:?}",df.height());
-println!("{:?}",df.head(Some(10)));
+    // let df = LazyCsvReader::new("./dataset/ratings_test.csv")
+    // .with_truncate_ragged_lines(true)
+    // .with_ignore_errors(true) // 파싱 오류 무시
+    // .with_rechunk(true)
+    // .with_skip_rows_after_header(0)
+    // .finish()
+    // .unwrap()
+    // .collect()
+    // .unwrap();
+    let df_csv = CsvReadOptions::default()
+        .with_infer_schema_length(None)
+        .with_has_header(true)
+        .with_parse_options(CsvParseOptions::default().with_truncate_ragged_lines(true))
+        .try_into_reader_with_file_path(Some("dataset/ratings_train.csv".into()))
+        .unwrap()
+        .finish()
+        .unwrap();
+    println!("{}", df_csv.height());
+    println!("{:?}",df_csv.shape());
+    println!("{:?}",df_csv.head(Some(3)));
 
     Ok(())
 }
